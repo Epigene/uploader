c49d0b55902ecfd23cf33f834cfdb64aecd4848e
Epigene <augusts.bautra@gmail.com>
2017-06-17 14:01:17 +0300

    Added admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 User.where(email: "augusts.bautra@gmail.com").first_or_initialize.update!(password: "1qaz2wsx")
+User.where(email: "admin@test.com").first_or_initialize.update!(password: "admin")
 
 puts "All seeded!"
